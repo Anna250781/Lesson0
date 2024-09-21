31c3a0fa5b808d1e0603c65ce35217b84fc00b1f
Anna250781 <ivanan81@mail.ru>
2024-09-21 20:25:00 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,36 +1,36 @@
 calls = 0
-def count_calls(calls):
-  #  if   ?????????????
-    calls += 1
+def count_calls():
     return calls
 
 
 my_tuple = ()
 def string_info(my_tuple):
-    global calls
     line = input("Введите название овоща: ")
     count = len(line)
     my_tuple = (count, line.upper(), line.lower())
-    count_calls(calls)
+    global calls
+    count_calls()
     print(my_tuple)
-
-
+    calls += 1
 
 string = True
 def is_contains(string):
-    global calles
     list_to_search = ["Алиса", "Светлана", "Ирина", "Вероника", "Татьяна"]
     name = input("Введите женское имя: ")
     for i in list_to_search:
         i == i.lower()
+        global calls
+        count_calls()
         if i == name.lower():
             string = True
+            calls += 1
         else:
             string = False
-    count_calls(calls)
+    calls += 1
     print(string)
 
 
+string_info(my_tuple)
 string_info(my_tuple)
 is_contains(string)
 print(calls)
