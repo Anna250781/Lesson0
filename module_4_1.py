8a7ded887332662a4b5bf85e53a4faa708a92f25
Anna250781 <ivanan81@mail.ru>
2024-10-15 13:07:49 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as dv
+from true_math import divide as dd
+
+
+result1 = dv(69, 3)
+result2 = dv(3, 0)
+result3 = dd(49, 7)
+result4 = dd(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        return inf
+    number2 = first / second
+    return number2
+
+
