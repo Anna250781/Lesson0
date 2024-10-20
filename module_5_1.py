0f65424aa041348ec0c275075c9f2805aec43094
Anna250781 <ivanan81@mail.ru>
2024-10-20 18:33:05 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,23 @@
+class House:
+    def __init__(self, name, num):
+        self.name = name
+        self.number_of_floors = num
+
+
+    def go_to(self, new_floor):
+        floor = int(new_floor)
+        f = 0
+        for i in range(1, floor + 1):
+            if floor > self.number_of_floors or  floor < 1:
+                print("Такого этажа не существует")
+                return
+            else:
+                f = f + 1
+                print(f)
+        return f
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
