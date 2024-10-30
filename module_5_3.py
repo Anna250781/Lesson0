6181c402f3ebf126a4c3143e9150c0ec1e8c63f7
Anna250781 <ivanan81@mail.ru>
2024-10-30 09:15:35 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -19,32 +19,43 @@ class House:
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
 
-    def __eq__(self, other:int):
-        return self.number_of_floors == other
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
-    def __lt__(self, other:int):
-        return self.number_of_floors < other
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
-    def __le__(self, other:int):
-        return self.number_of_floors <= other
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
 
-    def __gt__(self, other:int):
-        return self.number_of_floors > other
+    def __gt__(self, other):
+        return not self.__le__(other)
 
-    def _ge__(self, other:int):
-        return self.number_of_floors >= other
+    def __ge__(self, other):
+        return not self.__lt__(other)
 
-    def __ne__(self, other:int):
-        return self.number_of_floors != other
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
-    def __add__(self, value:int):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors + value}"
+    def __add__(self, value):
+        if isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        elif isinstance(value, int):
+             self.number_of_floors += value
+        return self
 
-    def __radd__(self, value:int):
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors + value}"
+    def __radd__(self, value):
+        return self.__add__(value)
 
-    def __iadd__(self, value: int):#- работают так же как и __add__ (возвращают результат его вызова).
-        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors + value}"
+    def __iadd__(self, value):#- работают так же как и __add__ (возвращают результат его вызова).
+        return self.__add__(value)
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -57,7 +68,7 @@ print(h1 == h2) # __eq__
 h1 = h1 + 10 # __add__
 print(h1)
 print(h1 == h2)
-h1=isinstance(h1.int)
+
 h1 += 10 # __iadd__
 print(h1)
 
